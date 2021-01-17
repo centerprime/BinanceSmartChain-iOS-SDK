b6dc9216160e5d65a3bdff99b2dca2b48b560b25
centerprime <support@centerprime.technology>
2021-01-17 21:08:36 +0900

    Initial commit

---
+++ Example/BinanceSmartChainSDK/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  BinanceSmartChainSDK
+//
+//  Created by x-oauth-basic on 01/17/2021.
+//  Copyright (c) 2021 x-oauth-basic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
